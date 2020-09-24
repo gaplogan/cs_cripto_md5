dfc1fe3e6009587fc976fb2d4177a14de658d0cb
Gabriel Artigas <gaplogan@gmail.com>
2020-09-24 19:33:37 -0300

    2020.09.24.19.33

---
+++ CriptoStringMD5/CriptoStringMD5/CriptoMD5.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+
+namespace CriptoStringMD5
+{
+    class CriptoMD5
+    {
+        public string RetornarMD5(string Senha)
+        {
+            using (MD5 md5Hash = MD5.Create())
+            {
+                return RetonarHash(md5Hash, Senha);
+            }
+        }
+
+        public bool CompararMD5(string senhaGuardada, string Senha_MD5)
+        {
+            string senha = RetornarMD5(senhaGuardada);
+            if (VerificarHash(Senha_MD5, senha))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private string RetonarHash(MD5 md5Hash, string input)
+        {
+            byte[] data = md5Hash.ComputeHash(Encoding.UTF8.GetBytes(input));
+
+            StringBuilder sBuilder = new StringBuilder();
+
+            for (int i = 0; i < data.Length; i++)
+            {
+                sBuilder.Append(data[i].ToString("X2"));
+            }
+
+            return sBuilder.ToString();
+        }
+
+        private bool VerificarHash(string input, string hash)
+        {
+            StringComparer compara = StringComparer.OrdinalIgnoreCase;
+
+            if (0 == compara.Compare(input, hash))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ CriptoStringMD5/CriptoStringMD5/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CriptoStringMD5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCriptografar_Click(object sender, EventArgs e)
+        {
+            CriptoMD5 md5 = new CriptoMD5();
+            txtSaida.Text = md5.RetornarMD5(txtEntrada.Text);
+        }
+
+        private void btnComparar_Click(object sender, EventArgs e)
+        {
+            CriptoMD5 md5 = new CriptoMD5();
+
+            string senhabanco = "81DC9BDB52D04DC20036DBD8313ED055";
+            string Senha = txtEntrada.Text;
+
+            bool CompararSenha = md5.CompararMD5(Senha, senhabanco);
+
+            if (CompararSenha)
+            {
+                labelResultado.Text = "Senhas Iguais";
+            }
+            else
+            {
+                labelResultado.Text = "Senhas Diferentes";
+            }
+        }
+    }
+}
